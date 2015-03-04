b0ee5959127e77b8c0645816186c3b512a2cddde
Ed Lewis <edward.richard.lewis@gmail.com>
2015-03-04 21:44:31 +0000

    nickel hello world

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nickel-bootstrap
+
 A seed implementation of nickel/bootstrap
+
+
+## Notes
+
+* would be helpful if docs listed what you need in your Cargo.toml
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![feature(net)]
+extern crate nickel;
+#[macro_use] extern crate nickel_macros;
+
+use std::net::IpAddr;
+use nickel::*;
+
 fn main() {
-    println!("Hello, world!");
+
+   let mut nickel = Nickel::new();
+
+   nickel.utilize(middleware! { |request|
+     println!("Request: {:?}", request.origin.uri);
+     });
+
+   nickel.utilize(router!(
+      get "**" => |request, response| {
+        "hello world"
+      }
+     ));
+
+   println!("Nickel is running.");
+   nickel.listen(IpAddr::new_v4(127,0,0,1), 6767);
 }
