0ef9e9eb3453aceb6daea3a26207fe9709a79dfd
Ed Lewis <edward.richard.lewis@gmail.com>
2015-03-05 00:18:14 +0000

    removed mustache partials - created one template with everything in. Works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![feature(net)]
 #![feature(path)]
+#![feature(fs)]
+#![feature(io)]
+
 extern crate nickel;
 #[macro_use] extern crate nickel_macros;
 extern crate mustache;
@@ -8,6 +11,8 @@ use std::collections::HashMap;
 use std::net::IpAddr;
 use nickel::*;
 use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
 use mustache::*;
 
 fn main() {
@@ -27,10 +32,18 @@ fn main() {
       data.insert("title".to_string(), StrVal("nickel-bootstrap".to_string()));
       data.insert("message".to_string(), StrVal("hello from nickel bootstrap!".to_string()));
 
-      let template = Context::new(PathBuf::new("mustache"))
-          .compile_path(PathBuf::new("base"))
+      let template_file = "templates/all_for_one.mustache";
+
+      let mut file = File::open(&PathBuf::new(template_file))
+                          .ok()
+                          .expect(&*format!("Couldn't open template: {}", template_file));
+
+      let mut raw_template = String::new();
+      file.read_to_string(&mut raw_template)
           .ok()
-          .unwrap();
+          .expect(&*format!("Couldn't read template file: {}", template_file));
+
+      let template = mustache::compile_str(&*raw_template);
 
       let mut resp = vec![];
       template.render_data(&mut resp, &Map(data));
