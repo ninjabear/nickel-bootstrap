742b31cae0fea29b3ed21dec9b5041a755e66eb6
Kevin Butler <haqkrs@gmail.com>
2015-03-05 21:21:22 +0000

    Simplify template rendering

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 #![feature(net)]
-#![feature(path)]
-#![feature(fs)]
-#![feature(io)]
-
 extern crate nickel;
 #[macro_use] extern crate nickel_macros;
 extern crate mustache;
+extern crate "rustc-serialize" as rustc_serialize;
 
 use std::collections::HashMap;
 use std::net::IpAddr;
 use nickel::*;
-use std::path::PathBuf;
-use std::fs::File;
-use std::io::Read;
-use mustache::*;
+// use mustache::*;
+use rustc_serialize::Encodable;
+
+
+struct View<T>(&'static str, T);
+
+impl<T: Encodable> ResponseFinalizer for View<T> {
+    fn respond<'a>(self, res: Response<'a>) -> MiddlewareResult<'a> {
+        let View(path, data) = self;
+        let stream = try!(res.render(path, &data));
+
+        Ok(Halt(stream))
+    }
+}
 
 fn main() {
-
    let mut nickel = Nickel::new();
 
    nickel.utilize(StaticFilesHandler::new("public/"));
@@ -29,26 +35,11 @@ fn main() {
 
    get "**" => |request, response| {
       let mut data = HashMap::new();
-      data.insert("title".to_string(), StrVal("nickel-bootstrap".to_string()));
-      data.insert("message".to_string(), StrVal("Welcome to".to_string()));
-      data.insert("message_line_two".to_string(), StrVal("You can edit this to get started...".to_string()));
+      data.insert("title", "nickel-bootstrap");
+      data.insert("message", "Welcome to");
+      data.insert("message_line_two", "You can edit this to get started...");
 
-      let template_file = "templates/all_for_one.mustache";
-
-      let mut file = File::open(&PathBuf::new(template_file))
-                          .ok()
-                          .expect(&*format!("Couldn't open template: {}", template_file));
-
-      let mut raw_template = String::new();
-      file.read_to_string(&mut raw_template)
-          .ok()
-          .expect(&*format!("Couldn't read template file: {}", template_file));
-
-      let template = mustache::compile_str(&*raw_template);
-
-      let mut resp = vec![];
-      template.render_data(&mut resp, &Map(data));
-      String::from_utf8(resp).unwrap()
+      View("templates/all_for_one.mustache", data)
    }
 
    ));
