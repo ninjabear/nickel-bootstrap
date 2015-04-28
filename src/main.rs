2502368f86406b39114a1fc87398c5ec03d15bbd
Ed Lewis <edward.richard.lewis@gmail.com>
2015-04-28 23:24:45 +0100

    get nickel-bootstrap working with nickel 0.2.1 crate (it doesn't)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-#![feature(net)]
+#[macro_use]
 extern crate nickel;
-#[macro_use] extern crate nickel_macros;
-extern crate "rustc-serialize" as rustc_serialize;
+extern crate rustc_serialize;
 
 use std::collections::HashMap;
 use std::net::IpAddr;
@@ -9,38 +8,30 @@ use nickel::*;
 use rustc_serialize::Encodable;
 
 
-struct View<T>(&'static str, T);
-
-impl<T: Encodable> ResponseFinalizer for View<T> {
-    fn respond<'a>(self, res: Response<'a>) -> MiddlewareResult<'a> {
-        let View(path, data) = self;
-        let stream = try!(res.render(path, &data));
-
-        Ok(Halt(stream))
-    }
-}
-
 fn main() {
+
    let mut nickel = Nickel::new();
+   let mut router = Nickel::router();
+
+   router.get("**", middleware! { |req, mut resp|
+
+     let mut data = HashMap::<&str, &str>::new();
+
+     data.insert("title", "nickel-bootstrap");
+     data.insert("message", "Welcome to");
+     data.insert("message_line_two", "You can edit this to get started...");
+
+     resp.render("templates/all_for_one.mustache", data);
+
+  });
+
+   nickel.utilize(router);
 
    nickel.utilize(StaticFilesHandler::new("public/"));
 
    nickel.utilize(middleware! { |request|
      println!("Request: {:?}", request.origin.uri);
-     });
+   });
 
-   nickel.utilize(router!(
-
-   get "**" => |request, response| {
-      let mut data = HashMap::new();
-      data.insert("title", "nickel-bootstrap");
-      data.insert("message", "Welcome to");
-      data.insert("message_line_two", "You can edit this to get started...");
-
-      View("templates/all_for_one.mustache", data)
-   }
-
-   ));
-
-   nickel.listen(IpAddr::new_v4(127,0,0,1), 6767);
+   nickel.listen("127.0.0.1:6767");
 }
