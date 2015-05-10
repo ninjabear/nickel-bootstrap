af894275baa0a902694646c36cc3d3691e7056ff
Ed Lewis <edward.richard.lewis@gmail.com>
2015-05-10 02:23:27 +0100

    remove commmented out code

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,6 @@ fn main() {
 
    let mut nickel = Nickel::new();
 
-   /*fn handler<'a>(_: &mut Request, res: Response<'a>) -> MiddlewareResult<'a> {
-     let mut data = HashMap::<&str, &str>::new();
-     data.insert("title", "nickel-bootstrap");
-     data.insert("message", "Welcome to");
-     data.insert("message_line_two", "You can edit this to get started...");
-
-     res.render("templates/all_for_one.mustache", &data);
-   }*/
-
    fn handler<'a>(_: &mut Request, res: Response<'a>) -> MiddlewareResult<'a> {
      let mut data = HashMap::<&str, &str>::new();
      data.insert("title", "nickel-bootstrap");
